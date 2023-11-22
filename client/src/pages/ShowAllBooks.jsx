710c6bdbddb03b4fc3357025ae7db85861077910
Tazim381 <jucse28.381@gmail.com>
2023-11-23 00:02:53 +0600

    filter option added

--- client/src/pages/ShowAllBooks.jsx
+++ client/src/pages/ShowAllBooks.jsx
@@ -3,10 +3,13 @@ import React, { useEffect, useState } from 'react';
 import BookList from '../components/BookList';
 import { AiOutlineSearch } from 'react-icons/ai';
 import SearchBook from '../components/SearchBook';
+import ShowBookByCategory from '../components/ShowBookByCategory';
 
 const ShowAllBooks = () => {
   const [books, setBooks] = useState([]);
-  const [searchQuery,setSearchQuery] = useState('')
+  const [searchQuery, setSearchQuery] = useState('')
+  const [categories, setCategories] = useState([])
+  const [selectedBooks, setSelectedBooks] = useState([])
 
   useEffect(() => {
     fetch('http://localhost:5000/api/book/allBooks')
@@ -14,34 +17,46 @@ const ShowAllBooks = () => {
       .then((data) => {
         setBooks(data);
       });
-  }, [books]); 
+  }, [books]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/book/uniqueCategories')
+      .then(response => response.json())
+      .then(data => setCategories(data))
+  }, [categories])
 
   return (
     <div className='mb-10'>
       <div className='flex items-center justify-center mt-10'>
-        
+
         <div className='flex gap-10 relative w-5/6'>
-          <select className='h-10 border border-teal-400 rounded-md pl-5 focus:outline-none '>
-            <option disabled>Filter By Category</option>
-            <option>উপন্যাস</option>
-            <option>নাটক</option>
-            <option>ছোট গল্প</option>
-            <option>কবিতা</option>
+          <select className='h-10 border border-teal-400 rounded-md pl-5 focus:outline-none'
+            onChange={(e) => setSelectedBooks(e.target.value)}>
+            <option value={''}>ALL</option>
+            {
+              categories.map((category) => (
+                <option value={category}>{category}</option>
+              ))
+            }
           </select>
           <input
             className='w-full h-10 pl-5 pr-12 border border-teal-400 rounded-md focus:outline-none'
             placeholder='Search By Book Name'
             value={searchQuery}
-            onChange={(e)=> setSearchQuery(e.target.value)}
+            onChange={(e) => setSearchQuery(e.target.value)}
           />
           <div className='absolute inset-y-0 right-0 flex items-center pr-3 '>
             <AiOutlineSearch className='text-teal-500 text-2xl cursor-pointer' />
           </div>
         </div>
       </div>
-      {
-        searchQuery?<SearchBook books={books} searchQuery={searchQuery}/>:<BookList books={books} />
-      }
+      {selectedBooks.length > 0 ? (
+        <ShowBookByCategory books={books} selectedBooks={selectedBooks} />
+      ) : searchQuery ? (
+        <SearchBook books={books} searchQuery={searchQuery} />
+      ) : (
+        <BookList books={books} />
+      )}
     </div>
   );
 };
