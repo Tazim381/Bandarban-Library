7b27a096d75ba78bea488f6e9993506bf21f0515
Parvej-Hoque-Palash <cloudnumber981@gmail.com>
2023-11-24 00:47:17 +0600

    added forms and uodate

--- client/src/pages/UpdateBooks.jsx
+++ client/src/pages/UpdateBooks.jsx
@@ -35,39 +35,65 @@ const UpdateBooks = () => {
       });
   }
   return (
-    <div className=" bg-teal-100 pb-16 overflow-hidden flex items-center justify-center">
+    <div className=" bg-slate-100 pb-16 mt-16 overflow-hidden flex items-center justify-center">
       <div className="bg-white lg:w-6/12 md:7/12  rounded-xl mt-10 mb-10">
         <form className="p-8 " onSubmit={handleSubmit}>
-          <div className=' content-center text-3xl font-bold mb-4 md:mb-6 text-gray-800'>Update Book Information</div>
-          <div className="flex items-center text-lg mb-4 md:mb-6">
-            <input defaultValue={data.bookName} type="text" name="bookName" className="bg-gray-200 rounded pl-12 py-1 md:py-2 focus:outline-none w-full" placeholder="Book Name" required />
+          <div className=' content-center text-2xl font-bold mb-4 md:mb-6 text-gray-800'>Update Book Information</div>
+          <div className='flex justify-between'>
+          <div className="flex flex-col items-start text-lg mb-4 md:mb-6">
+          <label class="block text-gray-700 text-sm mb-2" for="bookName">
+        Book Name
+      </label>
+            <input defaultValue={data.bookName} type="text" name="bookName" className="border-slate-300 hover:border-slate-500 pl-2 border-2 rounded py-1 md:py-2 focus:outline-none w-full" placeholder="Book Name" required />
           </div>
-          <div className="flex items-center text-lg mb-4 md:mb-6">
-            <input defaultValue={data.authorName} type="text" name="authorName" className="bg-gray-200 rounded pl-12 py-1 md:py-2 focus:outline-none w-full" placeholder="Author Name" required />
+          <div className="flex flex-col items-start text-lg mb-4 md:mb-6">
+          <label class="block text-gray-700 text-sm mb-2" for="authorName">
+        Author Name
+      </label>
+            <input defaultValue={data.authorName} type="text" name="authorName" className="border-slate-300 hover:border-slate-500 pl-2 border-2 rounded py-1 md:py-2 focus:outline-none w-full" placeholder="Author Name" required />
           </div>
-          <div className="flex items-center text-lg mb-4 md:mb-6">
-            <input defaultValue={data.publishedYear} type="text" name="publishedYear" className="bg-gray-200 rounded pl-12 py-1 md:py-2 focus:outline-none w-full" placeholder="Published Year" required />
           </div>
-          <div className="flex items-center text-lg mb-4 md:mb-6">
-            <input defaultValue={data.category} type="text" name="category" className="bg-gray-200 rounded pl-12 py-1 md:py-2 focus:outline-none w-full" placeholder="Book Category" required />
+          <div className='flex justify-between'>
+          <div className="flex flex-col items-start text-lg mb-4 md:mb-6">
+          <label class="block text-gray-700 text-sm mb-2" for="publishedYear">
+          Published Year
+      </label>
+            <input defaultValue={data.publishedYear} type="text" name="publishedYear" className="border-slate-300 hover:border-slate-500 pl-2 border-2 rounded py-1 md:py-2 focus:outline-none w-full" placeholder="Published Year" required />
           </div>
-          <div className="flex items-center text-lg mb-4 md:mb-6">
-            <select name="bookLanguage" className="bg-gray-200 rounded pl-2 py-1 md:py-2 focus:outline-none w-full" required>
+          <div className="flex flex-col items-start text-lg mb-4 md:mb-6">
+          <label class="block text-gray-700 text-sm mb-2" for="bookCategory">
+          Book Category
+      </label>
+            <input defaultValue={data.category} type="text" name="category" className="border-slate-300 hover:border-slate-500 pl-2 border-2 rounded py-1 md:py-2 focus:outline-none w-full" placeholder="Book Category" required />
+          </div>
+          </div>
+          <div className='flex justify-between'>
+          <div className="flex flex-col items-start text-lg mb-4 md:mb-6">
+          <label class="block text-gray-700 text-sm mb-2" for="bookLanguage">
+          Book Language
+      </label>
+            <select name="bookLanguage" className="border-slate-300 hover:border-slate-500 pl-2 border-2 rounded pl-2 py-1 md:py-2 focus:outline-none w-full" required>
               <option value="" disabled>Select Book Language</option>
               <option value="bangla">Bangla</option>
               <option value="english">English</option>
             </select>
           </div>
-
-          <div className="flex items-center text-lg mb-4 md:mb-6">
-            <input defaultValue={data.image} type="text" name="image" className="bg-gray-200 rounded pl-12 py-1 md:py-2 focus:outline-none w-full" placeholder="Image URL" required />
-          </div>
-          <div className="flex items-center text-lg mb-4 md:mb-6">
-          <select name="entryLanguage" className="bg-gray-200 rounded pl-2 py-1 md:py-2 focus:outline-none w-full" required>
+          <div className="flex flex-col items-start text-lg mb-4 md:mb-6">
+          <label class="block text-gray-700 text-sm mb-2" for="entryLanguage">
+          Entry Language
+      </label>
+          <select name="entryLanguage" className="border-slate-300 hover:border-slate-500 pl-2 border-2 rounded pl-2 py-1 md:py-2 focus:outline-none w-full" required>
               <option value="" disabled>Select Book Language</option>
               <option value="bangla">Bangla</option>
               <option value="english">English</option>
             </select>
+          </div>
+          </div>
+          <div className="flex flex-col items-start text-lg mb-4 md:mb-6">
+          <label class="block text-gray-700 text-sm mb-2" for="image">
+          Image URL
+      </label>
+            <input defaultValue={data.image} type="text" name="image" className="border-slate-300 hover:border-slate-500 pl-2 border-2 rounded py-1 md:py-2 focus:outline-none w-full" placeholder="Image URL" required />
           </div>
           <button type="submit" className="border-gray-50 bg-gradient-to-b from-cyan-700 to-cyan-900 font-medium px-2 py-1 md:px-1 md:py-2 text-white w-1/4 rounded">Update Book</button>
         </form>
