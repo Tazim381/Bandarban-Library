9fc80069c8ce46aa955e186a182881895e539d43
Parvej-Hoque-Palash <cloudnumber981@gmail.com>
2023-11-23 21:25:30 +0600

    added secure route

---
+++ client/src/routes/SecureRoute.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Auth } from "../hooks/Auth";
+
+export const SecureRoute = ({children}) => {
+  const { isAuthenticated } = Auth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isAuthenticated()) {
+      navigate('/login');
+    }
+
+  }, [isAuthenticated]);
+
+  if (isAuthenticated()) {
+    return children;
+  }
+  return null;
+}
