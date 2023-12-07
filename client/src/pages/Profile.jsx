cce393315f6297bb0ee427488ce681464a459cf4
Parvej-Hoque-Palash <cloudnumber981@gmail.com>
2023-12-07 13:37:18 +0600

    added profile design

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 const Profile = () => {
   return (
+<<<<<<< HEAD
     <div class="bg-white max-w-2xl shadow overflow-hidden sm:rounded-lg mt-24 ml-10">
     <div class="px-4 py-5 sm:px-6">
         <h3 class="text-lg leading-6 font-medium text-gray-900">
@@ -50,6 +51,69 @@ const Profile = () => {
                     About
                 </dt>
                 <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+=======
+    <div className="flex ml-auto mr-auto bg-white max-w-2xl shadow-[0_35px_60px_-15px_rgba(0,0,0,0.2)] overflow-hidden sm:rounded-lg mt-24 ml-10">
+    <div className="flex flex-col px-4 py-5 sm:px-6">
+    <div className=''>
+            <img className='rounded-full' src="https://i.ibb.co/Q9K8Hx9/images.jpg" alt="" />
+        </div>
+        <div className=''>
+        <h3 className="text-lg leading-6 font-medium text-gray-900">
+            User Profile
+        </h3>
+        <p className="mt-1 max-w-2xl text-sm text-gray-500">
+            Founding member
+        </p>
+        </div>
+    </div>
+    <div className="border-t border-gray-200">
+        <dl>
+            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">
+                    Full name
+                </dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    Alice Bob
+                </dd>
+            </div>
+            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">
+                    Age
+                </dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    25
+                </dd>
+            </div>
+            <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">
+                    Position
+                </dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    Founder
+                </dd>
+            </div>
+            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">
+                    Email address
+                </dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    m.poul@example.com
+                </dd>
+            </div>
+            <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">
+                    Phone
+                </dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    01923433544
+                </dd>
+            </div>
+            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">
+                    About
+                </dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+>>>>>>> 0c6da9c (added profile design)
                     To get social media testimonials like these, keep your customers engaged with your social media accounts by posting regularly yourself
                 </dd>
             </div>
