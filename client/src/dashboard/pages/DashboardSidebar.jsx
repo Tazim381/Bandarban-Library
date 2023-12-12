db2a391dea7a775071e7b27093743f36bf8cb3a8
Tazim381 <jucse28.381@gmail.com>
2023-12-12 19:19:00 +0600

    dashboard data added

--- client/src/dashboard/pages/DashboardSidebar.jsx
+++ client/src/dashboard/pages/DashboardSidebar.jsx
@@ -3,6 +3,7 @@ import { Link, useNavigate } from 'react-router-dom'
 import { AiOutlineUser } from "react-icons/ai"
 import { IoMdPersonAdd } from "react-icons/io"
 import { IoMdExit } from "react-icons/io";
+import { LuLayoutDashboard } from "react-icons/lu";
 
 
 const DashboardSidebar = () => {
@@ -33,7 +34,10 @@ const DashboardSidebar = () => {
           sidebarOpen ? 'translate-x-0' : '-translate-x-full'
         } transform transition-transform ease-in-out duration-300 fixed flex flex-col gap-y-3 bg-teal-900 w-64 px-5 text-white top-0 left-0 bottom-0 pt-2 mt-16 md:translate-x-0`}
       >
-        <Link className=' px-4 py-2 rounded-md hover:bg-teal-700' to='/dashboard'>Dashboard</Link>
+        <div className='flex items-center gap-x-1 px-4 py-2 rounded-md hover:bg-teal-700'>
+        <LuLayoutDashboard/>
+        <Link  to='/dashboard'>Dashboard</Link>
+        </div>
         <div className='flex items-center gap-x-1 px-4 py-2 rounded-md hover:bg-teal-700'>
           <AiOutlineUser />
           <Link to='/dashboard/profile'>Profile</Link>
