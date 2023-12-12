47a9e1c4e9559b020f74050cf0b337c3ce18d39e
Tazim381 <jucse28.381@gmail.com>
2023-12-12 19:19:00 +0600

    dashboard data added

--- client/src/dashboard/pages/DashBoardHome.jsx
+++ client/src/dashboard/pages/DashBoardHome.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+//dashboardItems
 
 const DashBoardHome = () => {
+  const [totalBooks,setTotalBooks] = useState('')
+  const [totalWriters,setTotalWriters] = useState('')
+  const [totalFoundingMembers, setTotalFoundingMembers] = useState('')
+  const [totalAdmins, setTotalAdmins] = useState('')
+  
+  useEffect(()=>{
+    fetch('http://localhost:5000/api/book/dashboardItems')
+    .then((response)=> response.json())
+    .then((data)=> {
+      setTotalBooks(data.totalBooks)
+      setTotalWriters(data.totalWriters)
+      setTotalFoundingMembers(data.totalFoundingMembers)
+      setTotalAdmins(data.totalAdmins)
+    })
+  })
   return (
-    <div className='ms-64 mt-36'>DashBoardHome</div>
+
+    <div className='ms-64 mt-36'>
+    {
+       <div className='ms-64 mt-36'>
+       <p>Total Books: {totalBooks}</p>
+       <p>Total Writers: {totalWriters}</p>
+       <p>Total FoundingMembes : {totalFoundingMembers}</p>
+       <p>Total Admins : {totalAdmins}</p>
+       </div>
+    }
+    </div>
   )
 }
 
